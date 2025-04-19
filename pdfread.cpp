64eb393a4baf6d2be6a6895bea4093e9af66250e
acestandard220 <86978418+acestandard220@users.noreply.github.com>
2025-04-19 15:44:09 +0000

    Fixed spacing

--- pdfread.cpp
+++ pdfread.cpp
@@ -4,143 +4,137 @@
 #include <iostream>
 #include <ostream>
 #include <string>
-int m =9;
+int m = 9;
 
-namespace PDFREAD{
+namespace PDFREAD {
 
-  std::array<const std::string, 10> type_string{ "/Catalog","/Pages","Page","endobj"};
-  std::unordered_map<char, char> pair = { {'[',']'},{'{','}'},{'(',')'}};
-  filedata* data;
+    std::array<const std::string, 10> type_string{ "/Catalog", "/Pages", "Page", "endobj" };
+    std::unordered_map<char, char> pair = { {'[', ']'}, {'{', '}'}, {'(', ')'} };
+    filedata* data;
 
-  int get_length_to(std::string& line, int start , char stop = ' ')
-  {
-    int x = 0;
-    for(int i = 0;i < line.length() - start; i++)
+    int get_length_to(std::string& line, int start, char stop = ' ')
     {
-      if(line[i + start] != stop)
-      {
-        x++;
-      }
-      else {
-        break;  
-      }
+        int x = 0;
+        for (int i = 0; i < line.length() - start; i++)
+        {
+            if (line[i + start] != stop)
+            {
+                x++;
+            }
+            else {
+                break;
+            }
+        }
+        return x;
     }
-    return x;
-  }
 
-  std::vector<int> get_array_objs(std::string& line, int start) //todo: merge with beta idea
-  {
-      std::vector<int> r;
-      char h = line[start];
-      int objs_start = start + 1;//Plus 1 to skip the opening braces
+    std::vector<int> get_array_objs(std::string& line, int start) //todo: merge with beta idea
+    {
+        std::vector<int> r;
+        char h = line[start];
+        int objs_start = start + 1; //Plus 1 to skip the opening braces
 
-      if (pair.find(h) == pair.end())
-      {
-          std::cout << "[ERROR]:[SYNTHAX]:::Invalid PDF Container Synthax...\n";
-          return std::vector<int>();
-      }
+        if (pair.find(h) == pair.end())
+        {
+            std::cout << "[ERROR]:[SYNTHAX]:::Invalid PDF Container Synthax...\n";
+            return std::vector<int>();
+        }
 
-      
-      int a = get_length_to(line, start, pair[h]);  //Get length to its closing brackets...
-      std::string linear_data = line.substr(objs_start, a); //Store length to its closing brackets
-      int T = linear_data.length() - 1; //Remove \n
+        int a = get_length_to(line, start, pair[h]);  //Get length to its closing brackets...
+        std::string linear_data = line.substr(objs_start, a); //Store length to its closing brackets
+        int T = linear_data.length() - 1; //Remove \n
 
-      //Formula for finding number of objects in array
-      int tD = T / 5;
-      int tDD = static_cast<int>(tD/5);
-      int nObjs = static_cast<int>(tD) - tDD;
+        //Formula for finding number of objects in array
+        int tD = T / 5;
+        int tDD = static_cast<int>(tD / 5);
+        int nObjs = static_cast<int>(tD) - tDD;
 
-      for (int i = 0; i < nObjs * 6; i+=6)
-      {
-          int index = i + objs_start;
-          int int_length = get_length_to(line, index);
-          r.push_back(std::stoi(line.substr(index, int_length)));
-      }
-      return r;
-  }
+        for (int i = 0; i < nObjs * 6; i += 6)
+        {
+            int index = i + objs_start;
+            int int_length = get_length_to(line, index);
+            r.push_back(std::stoi(line.substr(index, int_length)));
+        }
+        return r;
+    }
 
-  std::vector<int> get_array_objs2(std::string& line, int start,int obj_blocks,int spacing = 1) //todo:beta
-  {
-      std::vector<int> r;
-      char h = line[start];
-      int objs_start = start + 1;//Plus 1 to skip the opening braces
+    std::vector<int> get_array_objs2(std::string& line, int start, int obj_blocks, int spacing = 1) //todo:beta
+    {
+        std::vector<int> r;
+        char h = line[start];
+        int objs_start = start + 1; //Plus 1 to skip the opening braces
 
-      if (pair.find(h) == pair.end())
-      {
-          std::cout << "[ERROR]:[SYNTHAX]:::Invalid PDF Container Synthax...\n";
-          return std::vector<int>();
-      }
+        if (pair.find(h) == pair.end())
+        {
+            std::cout << "[ERROR]:[SYNTHAX]:::Invalid PDF Container Synthax...\n";
+            return std::vector<int>();
+        }
 
+        int a = get_length_to(line, start, pair[h]);  //Get length to its closing brackets...
+        std::string linear_data = line.substr(objs_start, a); //Store length to its closing brackets
+        int T = linear_data.length() - 1; //Remove \n
 
-      int a = get_length_to(line, start, pair[h]);  //Get length to its closing brackets...
-      std::string linear_data = line.substr(objs_start, a); //Store length to its closing brackets
-      int T = linear_data.length() - 1; //Remove \n
+        //Formula for finding number of objects in array
+        int tD = T / obj_blocks;
+        int tDD = static_cast<int>(tD / obj_blocks);
+        int nObjs = static_cast<int>(tD) - tDD;
 
-      //Formula for finding number of objects in array
-      int tD = T / obj_blocks;
-      int tDD = static_cast<int>(tD / obj_blocks);
-      int nObjs = static_cast<int>(tD) - tDD;
+        for (int i = 0; i < nObjs * obj_blocks + spacing; i += obj_blocks)
+        {
+            int index = i + objs_start;
+            int int_length = get_length_to(line, index);
+            r.push_back(std::stoi(line.substr(index, int_length)));
+        }
+        return r;
+    }
 
-      for (int i = 0; i < nObjs * obj_blocks + spacing; i += obj_blocks)
-      {
-          int index = i + objs_start;
-          int int_length = get_length_to(line, index);
-          r.push_back(std::stoi(line.substr(index, int_length)));
-      }
-      return r;
-  }
+    int has_key(std::string& line, const std::string& key)
+    {
+        int line_index = line.find(key);
+        int search_length = key.length();
+        if (line.find(key) != std::string::npos)
+        {
+            return line_index + search_length; //true
+        }
+        else
+        {
+            return 0; //false
+        }
+    }
 
-  int has_key(std::string& line, const std::string& key)
-  {
-      int line_index = line.find(key);
-      int search_length = key.length();
-      if (line.find(key) != std::string::npos)
-      {
-          return line_index + search_length; //true
-      }
-      else
-      {
-          return 0; //false
-      }
-  }
+    bool validate_obj_type(std::string& line, type_index type)
+    {
+        const std::string temp = "/Type ";
+        int start = has_key(line, temp);
 
-  bool validate_obj_type(std::string& line,type_index type)
-  {
-      const std::string temp = "/Type ";
-      int start = has_key(line, temp);
+        int x = get_length_to(line, start);
+        std::string sub = line.substr(start, x);
+        if (sub.find(type_string[type]) == std::string::npos)
+        {
+            std::cout << "[ERROR]:[XREF CORRUPT]:: Invalid " << type_string[type] << " Offset...\n";
+            return false;
+        }
 
-      int x = get_length_to(line, start);
-      std::string sub = line.substr(start, x);
-      if (sub.find(type_string[type]) == std::string::npos)
-      {
-          std::cout << "[ERROR]:[XREF CORRUPT]:: Invalid "<< type_string[type] <<" Offset...\n";
-          return false;
-      }
+        return true;
+    }
 
-      return true;
-  }
+    bool end_of_obj(std::string& line)
+    {
+        return (line.find(type_string[ENDOBJ]) != std::string::npos);
+    }
 
-  bool end_of_obj(std::string& line)
-  {
-      return (line.find(type_string[ENDOBJ]) != std::string::npos);
-  }
-
- //TODO: find a way to not read to the end of the file to avoid using flags to tell what has been done and what hasn't been done
-  //If  you find the way, you won't have to worry about reading keys from other objects
-  //TODO: Done but has to be tested...
     void read_page_data(std::ifstream& file)
     {
         std::string line;
         for (auto& page_obj_index : data->root->pages->mPages)
         {
             file.clear();
-            file.seekg(data->obj_offsets[page_obj_index][0],std::ios::beg);
-            
-            
-            while (std::getline(file,line))
+            file.seekg(data->obj_offsets[page_obj_index][0], std::ios::beg);
+
+            while (std::getline(file, line))
             {
                 if (end_of_obj(line)) { break; }
-                int start = has_key(line,"/Type ");
+                int start = has_key(line, "/Type ");
                 if (start)
                 {
                     if (!validate_obj_type(line, PAGE))
@@ -148,39 +142,37 @@ namespace PDFREAD{
                         return;
                     }
                 }
-                
+
                 start = has_key(line, "/MediaBox ");
                 if (start)
                 {
-                    //get_array_objs2(line,start,)
+                    //get_array_objs2(line, start)
                 }
-                
-            
             }
         }
     }
 
     void read_page_collector(std::ifstream& file)
-    {  
+    {
         file.seekg(data->obj_offsets[data->root->pages->id][0]);
         std::string line;
-    
-        while(std::getline(file,line))
+
+        while (std::getline(file, line))
         {
-          if (end_of_obj(line)) { break; }
-          std::string search = "/Type ";
-          int start = has_key(line, search);
-    
-            if(start)
+            if (end_of_obj(line)) { break; }
+            std::string search = "/Type ";
+            int start = has_key(line, search);
+
+            if (start)
             {
                 if (!validate_obj_type(line, PAGES))
                 {
                     return;
                 }
-            }    
-            search = "/Count ";  
+            }
+            search = "/Count ";
             start = has_key(line, search);
-            if(start)
+            if (start)
             {
                 int x = get_length_to(line, start);
                 data->root->pages->nPages = std::stoi(line.substr(start, x));
@@ -203,154 +195,152 @@ namespace PDFREAD{
             }
             std::string subbed = line.substr(start);
         }
-    }  
+    }
 
     // add breaks when we are certain we dont need anything else;
     void read_root_obj(std::ifstream& file)
-    { 
+    {
         file.clear();
-      file.seekg(data->obj_offsets[data->root->id][0]);
-      std::string line;
-      
-        while(std::getline(file,line))
+        file.seekg(data->obj_offsets[data->root->id][0]);
+        std::string line;
+
+        while (std::getline(file, line))
         {
-              if (line.find(type_string[ENDOBJ]) != std::string::npos)
-              {
-                  std::cout << "End of object reached\n";
-                  break;
-              }
-           
+            if (line.find(type_string[ENDOBJ]) != std::string::npos)
+            {
+                std::cout << "End of object reached\n";
+                break;
+            }
+
             std::string search = "/Type ";
             int start = has_key(line, search);
-           
+
             if (start)
             {
-                if (!validate_obj_type(line,CATALOG))
+                if (!validate_obj_type(line, CATALOG))
                 {
                     return;
                 }
-                
             }
-            
-           
+
             data->root->pages = new page_collection;
             search = "/Pages ";
             start = has_key(line, search);
-           
-            if(start)
-            {  
-              data->root->pages->id = std::stoi(line.substr(start,get_length_to(line,start)));
-              std::cout << "Page Collector ID::: "<< data->root->pages->id <<std::endl;
-              break;
+
+            if (start)
+            {
+                data->root->pages->id = std::stoi(line.substr(start, get_length_to(line, start)));
+                std::cout << "Page Collector ID::: " << data->root->pages->id << std::endl;
+                break;
             }
         }
     }
-  //todo: Integrate With read_trailer_info
-  void read_start_xref(std::ifstream* file)
-  {
-    std::string line;
-    size_t global_index = 0;
-    bool found = false;
-    while(std::getline(*file,line))
+
+    //todo: Integrate With read_trailer_info
+    void read_start_xref(std::ifstream* file)
     {
-      int line_length = line.length();
-      int line_index = line.find("startxref");
-     
-      if(line_index != std::string::npos)
-      {
-        found = true;     
-        continue;
-      }
-      if(found)
-      {
-        int value = std::stoi(line);
-        data->xref_start = value;
-        break;
-      }
+        std::string line;
+        size_t global_index = 0;
+        bool found = false;
+        while (std::getline(*file, line))
+        {
+            int line_length = line.length();
+            int line_index = line.find("startxref");
+
+            if (line_index != std::string::npos)
+            {
+                found = true;
+                continue;
+            }
+            if (found)
+            {
+                int value = std::stoi(line);
+                data->xref_start = value;
+                break;
+            }
+        }
     }
-  }
 
-  void read_trailer_info(std::ifstream* file)
-  {
-    file->seekg(0);
-    std::string line; 
-    while(std::getline(*file,line))
-    { 
-      std::string search = "/Size ";       
-      int start = has_key(line,search);
-      
-      if(start)
-      {  
-         if(file->tellg() < data->xref_start)
-         {
-             continue; //skip any /Size attribute before the xref...
-         }
-         int integer_length = get_length_to(line, start);
-         data->num_obj = std::stoi(line.substr(start,integer_length));
-         data->obj_offsets.resize(data->num_obj);
-         std::cout<<"Num_Obj::: "<<data->num_obj<<std::endl;
-         continue;
-      }
-      
-      search = "/Root ";
-      start = has_key(line, search);
-
-      if(start)
-      {
-        int integer_length = get_length_to(line, start);
-        data->root->id = std::stoi(line.substr(start, integer_length));
-        std::cout<<"ss:: "<<data->root->id<<std::endl;
-        break;
-      }
-    }
-  }
-  
-  void read_obj_offsets(std::ifstream& file)
-  {
-    std::string line;
-
-    file.seekg(data->xref_start); 
-    std::getline(file,line);//read: xref
-    std::getline(file,line);//read: 0-xxx 
-
-    for(int i = 0; i< data->num_obj;i++)
-    {  
-      std::getline(file,line);
-
-      data->obj_offsets[i][0] = std::stoi(line.substr(0,10));
-      data->obj_offsets[i][1] = std::stoi(line.substr(11,5));
-      data->obj_offsets[i][2] = (line.substr(17, 1) == "f") ? 0 : 1; //NOTE: f = 0 && n = 1
-    }
-  }
-
-  void Initialize()
-  {
-
-    std::ifstream file("samplepdf.pdf",std::ios::binary);
-    if(!file.is_open())
+    void read_trailer_info(std::ifstream* file)
     {
-      std::cout<<"Could not open file...\n";
+        file->seekg(0);
+        std::string line;
+        while (std::getline(*file, line))
+        {
+            std::string search = "/Size ";
+            int start = has_key(line, search);
+
+            if (start)
+            {
+                if (file->tellg() < data->xref_start)
+                {
+                    continue; //skip any /Size attribute before the xref...
+                }
+                int integer_length = get_length_to(line, start);
+                data->num_obj = std::stoi(line.substr(start, integer_length));
+                data->obj_offsets.resize(data->num_obj);
+                std::cout << "Num_Obj::: " << data->num_obj << std::endl;
+                continue;
+            }
+
+            search = "/Root ";
+            start = has_key(line, search);
+
+            if (start)
+            {
+                int integer_length = get_length_to(line, start);
+                data->root->id = std::stoi(line.substr(start, integer_length));
+                std::cout << "ss:: " << data->root->id << std::endl;
+                break;
+            }
+        }
     }
-    data = new filedata;
 
-    file.seekg(0,std::ios::end);
-    data->file_bytes = file.tellg();
-    file.seekg(0,std::ios::beg);
+    void read_obj_offsets(std::ifstream& file)
+    {
+        std::string line;
 
-    read_start_xref(&file);
+        file.seekg(data->xref_start);
+        std::getline(file, line); //read: xref
+        std::getline(file, line); //read: 0-xxx 
 
-    data->root = new root_node;
-      
-    read_trailer_info(&file);
-    read_obj_offsets(file);
+        for (int i = 0; i < data->num_obj; i++)
+        {
+            std::getline(file, line);
 
-    read_root_obj(file);
-    read_page_collector(file);
-    std::cout << "Data:pages::: " << data->root->pages->nPages;
-  }
- 
-  void Shutdonw()
-  {
+            data->obj_offsets[i][0] = std::stoi(line.substr(0, 10));
+            data->obj_offsets[i][1] = std::stoi(line.substr(11, 5));
+            data->obj_offsets[i][2] = (line.substr(17, 1) == "f") ? 0 : 1; //NOTE: f = 0 && n = 1
+        }
+    }
 
-  }
+    void Initialize()
+    {
+        std::ifstream file("samplepdf.pdf", std::ios::binary);
+        if (!file.is_open())
+        {
+            std::cout << "Could not open file...\n";
+        }
+        data = new filedata;
+
+        file.seekg(0, std::ios::end);
+        data->file_bytes = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        read_start_xref(&file);
+
+        data->root = new root_node;
+
+        read_trailer_info(&file);
+        read_obj_offsets(file);
+
+        read_root_obj(file);
+        read_page_collector(file);
+        std::cout << "Data:pages::: " << data->root->pages->nPages;
+    }
+
+    void Shutdonw()
+    {
+        // Empty function
+    }
 }
